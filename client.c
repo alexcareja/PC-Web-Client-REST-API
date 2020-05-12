d439699edb7e62625bae89535ed08e9e9604a8be
Alex Careja <alexandrucareja@yahoo.com>
2020-05-12 04:31:39 -0400

    Add files via upload

--- client.c
+++ client.c
@@ -10,6 +10,9 @@
 #include "helpers.h"
 #include "requests.h"
 
+#define POST "POST"
+#define GET "GET"
+#define DELETE "DELETE"
 #define REGISTER "register"
 #define LOGIN "login"
 #define ACCESS "enter_library"
@@ -114,40 +117,7 @@ int check_access() {
 void register_client() {
 	char *message;
 	char *response;
-	char type[5] = "POST";
-	char **body_data = (char **) malloc(sizeof(char *));
-	body_data[0] = (char *) calloc(600, sizeof(char));
-	char *uname = (char *) calloc(256, sizeof(char));
-	char *pass = (char *) calloc(256, sizeof(char));
-	printf("username=");
-	scanf("%s", uname);
-	printf("password=");
-	scanf("%s", pass);
-   	sprintf(body_data[0], "{ \"username\": \"%s\",\n\"password\": \"%s\" }",
-   		uname, pass);
-	message = compute_request(type, url, REGISTER_PATH, APP_JSON, body_data,
-		1, NULL, NULL, 0);
-    send_to_server(sockfd, message);
-    response = receive_from_server(sockfd);
-    handle_error(response);
-   	free(uname);
-   	free(pass);
-   	free(message);
-   	free(response);
-   	free(body_data[0]);
-   	free(body_data);
-}
-
-void login() {
-	if (login_cookie != NULL) {
-		printf("You already are logged in.\n");
-		return;
-	}
-	char *message;
-	char *response;
-	char *aux;
-	char *cookie;
-	char type[5] = "POST";
+	char type[5] = POST;	// request type
 	char **body_data = (char **) malloc(sizeof(char *));
 	body_data[0] = (char *) calloc(600, sizeof(char));
 	char *uname = (char *) calloc(256, sizeof(char));
@@ -160,7 +130,45 @@ void login() {
 	// create JSON payload
    	sprintf(body_data[0], "{ \"username\": \"%s\", \"password\": \"%s\" }",
    		uname, pass);
-   	// create POST message
+   	// create POST request
+	message = compute_request(type, url, REGISTER_PATH, APP_JSON, body_data,
+		1, NULL, NULL, 0);
+	// send message to server
+    send_to_server(sockfd, message);
+    // get server response
+    response = receive_from_server(sockfd);
+    handle_error(response);
+   	free(uname);
+   	free(pass);
+   	free(message);
+   	free(response);
+   	free(body_data[0]);
+   	free(body_data);
+}
+
+void login() {
+	if (login_cookie != NULL) {	// check if already logged in
+		printf("You already are logged in.\n");
+		return;
+	}
+	char *message;
+	char *response;
+	char *aux;
+	char *cookie;
+	char type[5] = POST;	// request type
+	char **body_data = (char **) malloc(sizeof(char *));
+	body_data[0] = (char *) calloc(600, sizeof(char));
+	char *uname = (char *) calloc(256, sizeof(char));
+	char *pass = (char *) calloc(256, sizeof(char));
+	// ask for username and password
+	printf("username=");
+	scanf("%s", uname);
+	printf("password=");
+	scanf("%s", pass);
+	// create JSON payload
+   	sprintf(body_data[0], "{ \"username\": \"%s\", \"password\": \"%s\" }",
+   		uname, pass);
+   	// create POST request
 	message = compute_request(type, url, LOGIN_PATH, APP_JSON, body_data,
 		1, NULL, NULL, 0);
     // send message to server
@@ -168,6 +176,7 @@ void login() {
     // get server response
     response = receive_from_server(sockfd);
    	if (handle_error(response) == 0) {
+    	// request was successful
    		// look for "Set-Cookie"
    		aux = response;
    		while (strncmp(aux, "Set-Cookie", 10)) {
@@ -190,7 +199,7 @@ void login() {
 }
 
 void get_access() {
-	if (check_login()) {
+	if (check_login()) { // check if logged in
 		return;
 	}
 	if (jwt_token != NULL) {
@@ -201,24 +210,34 @@ void get_access() {
 	char *response;
 	char *token;
 	char *aux;
-	char type[4] = "GET";
+	char type[4] = GET;	// request type
 	char **cookies = (char **) malloc(sizeof(char *));
 	cookies[0] = (char *) calloc(200, sizeof(char));
+	// add the login cookie
 	strcpy(cookies[0], login_cookie);
+	// create GET request
 	message = compute_request(type, url, ACCESS_PATH, NULL, NULL, 0, NULL,
 		cookies, 1);
+	// send message to sever
     send_to_server(sockfd, message);
+    // get server response
     response = receive_from_server(sockfd);
+    // handle error if failed or get the jwt token if successful
     if (handle_error(response) == 0) {
+    	// request was successful
     	aux = response;
+    	// look for token keyword
    		while(strncmp(aux, "token", 5)) {
    			aux++;
    		}
    		aux += 8;
-   		token = strtok(aux, "}");
+   		// trim excess characters
+   		token = strtok(aux, "\"");
    		jwt_token = (char *) calloc(400, sizeof(char));
-   		strncpy(jwt_token, token, strlen(token) - 1);
+   		// copy token value
+   		strncpy(jwt_token, token, strlen(token));
     }
+   	// free allocated memory
    	free(message);
    	free(response);
    	free(cookies[0]);
@@ -226,29 +245,35 @@ void get_access() {
 }
 
 void get_books() {
-	if (check_login()) {
-		return;
-	}
 	if (check_access()) {
 		return;
 	}
 	char *message;
 	char *response;
 	char *aux;
-	char type[4] = "GET";
+	char type[4] = GET;	// request type
 	char **cookies = (char **) malloc(sizeof(char *));
 	cookies[0] = (char *) calloc(200, sizeof(char));
+	// add login cookie
 	strcpy(cookies[0], login_cookie);
-	message = compute_request(type, url, BOOKS_PATH, NULL, NULL, 0, jwt_token, cookies, 1);
+	// create GET request
+	message = compute_request(type, url, BOOKS_PATH, NULL, NULL, 0, jwt_token,
+		cookies, 1);
+	// send message to server
     send_to_server(sockfd, message);
+    // get server response
     response = receive_from_server(sockfd);
+    // handle error if failed or print books if successful
     if (handle_error(response) == 0) {
+    	// request was successful
    		aux = response;
+   		// search for start of JSON object payload
    		while (strncmp(aux, "[", 1)) {
    			aux++;
    		}
    		printf("%s\n", aux);
     }
+   	// free allocated memory
    	free(message);
    	free(response);
    	free(cookies[0]);
@@ -256,36 +281,61 @@ void get_books() {
 }
 
 void get_book() {
-	if (check_login()) {
-		return;
-	}
 	if (check_access()) {
 		return;
 	}
+	int i = 0, negative = 0;
 	char *message;
 	char *response;
 	char *path = (char *) calloc(200, sizeof(char));
 	char *aux;
-	char type[4] = "GET";
+	char type[4] = GET;	// request type
 	char id[10];
+	// ask for book id
 	printf("id=");
 	scanf("%s", id);
+	// check if id is a number
+	if (id[i] == '-') {
+		negative = 1;
+		i++;
+	}
+	for (; i < strlen(id); i++) {
+		if (isdigit(id[i]) == 0) {
+			// return and print message if NaN
+			printf("Request failed. 'id' must be a number.\n");
+			return;
+		}
+	}
+	if (negative) {
+		printf("Request failed. 'id' must be positive.\n");
+		return;
+	}
+	// adapt path
 	strcpy(path, BOOKS_PATH);
 	strcat(path, "/");
 	strcat(path, id);
 	char **cookies = (char **) malloc(sizeof(char *));
+	// add login cookie
 	cookies[0] = (char *) calloc(200, sizeof(char));
 	strcpy(cookies[0], login_cookie);
-	message = compute_request(type, url, path, NULL, NULL, 0, jwt_token, cookies, 1);
+	// compute GET request
+	message = compute_request(type, url, path, NULL, NULL, 0, jwt_token,
+		cookies, 1);
+	// send message to server
     send_to_server(sockfd, message);
+    // get server response
     response = receive_from_server(sockfd);
+    // handle error if failed or print book details if successful
     if (handle_error(response) == 0) {
+    	// request was successful
     	aux = response;
+    	// search for start of JSON payload
    		while (strncmp(aux, "[", 1)) {
    			aux++;
    		}
    		printf("%s\n", aux);
     }
+   	// free allocated memory
    	free(message);
    	free(response);
    	free(cookies[0]);
@@ -293,9 +343,6 @@ void get_book() {
 }
 
 void add_book() {
-	if (check_login()) {
-		return;
-	}
 	if (check_access()) {
 		return;
 	}
@@ -304,8 +351,9 @@ void add_book() {
 	int i;
 	char **body_data = (char **) malloc(sizeof(char *));
 	body_data[0] = (char *) calloc(600, sizeof(char));
-	char type[10] = "POST";
+	char type[10] = POST;	// request type
 	char title[100], author[100], genre[100], publisher[100], pages[10];
+	// ask for title, author, genre, publisher, and page_count
 	printf("title=");
 	scanf("%s", title);
 	printf("author=");
@@ -316,22 +364,32 @@ void add_book() {
 	scanf("%s", publisher);
 	printf("page_count=");
 	scanf("%s", pages);
+	// check if page_count is a number
 	for (i = 0; i < strlen(pages); i++) {
 		if (isdigit(pages[i]) == 0) {
+			// print message and return if failed
 			printf("Request failed. 'page_count' must be a number.\n");
 			return;
 		}
 	}
-	sprintf(body_data[0], "{ \"title\": \"%s\", \"author\": \"%s\", \"genre\": \"%s\", \"page_count\": \"%s\", \"publisher\": \"%s\" }",
+	// create JSON payload
+	sprintf(body_data[0],
+		"{ \"title\": \"%s\", \"author\": \"%s\", \"genre\": \"%s\", \"page_count\": \"%s\", \"publisher\": \"%s\" }",
    		title, author, genre, pages, publisher);
 	char **cookies = (char **) malloc(sizeof(char *));
 	cookies[0] = (char *) calloc(200, sizeof(char));
+	// add login cookie
 	strcpy(cookies[0], login_cookie);
+	// create POST request
 	message = compute_request(type, url, BOOKS_PATH, APP_JSON, body_data, 1,
 		jwt_token, cookies, 1);
+	// send message to server
     send_to_server(sockfd, message);
+    // get server response
     response = receive_from_server(sockfd);
+    // handle error if request failed
     handle_error(response);
+   	// free allocated memory
    	free(message);
    	free(response);
    	free(cookies[0]);
@@ -341,30 +399,44 @@ void add_book() {
 }
 
 void delete_book() {
-	if (check_login()) {
-		return;
-	}
 	if (check_access()) {
 		return;
 	}
+	int i;
 	char *message;
 	char *response;
 	char *path = (char *) calloc(200, sizeof(char));
-	char type[10] = "DELETE";
+	char type[10] = DELETE;	// request type
 	char id[10];
+	// ask for book id
 	printf("id=");
 	scanf("%s", id);
+	// check if id is a number
+	for (i = 0; i < strlen(id); i++) {
+		if (isdigit(id[i]) == 0) {
+			// print message and return if NaN
+			printf("Request failed. 'id' must be a number.\n");
+			return;
+		}
+	}
+	// adapt path
 	strcpy(path, BOOKS_PATH);
 	strcat(path, "/");
 	strcat(path, id);
 	char **cookies = (char **) malloc(sizeof(char *));
+	// add login cookie
 	cookies[0] = (char *) calloc(200, sizeof(char));
 	strcpy(cookies[0], login_cookie);
+	// compute DELETE request
 	message = compute_request(type, url, path, NULL, NULL, 0, jwt_token,
 		cookies, 1);
+	// send message to server
     send_to_server(sockfd, message);
+    // get server response
     response = receive_from_server(sockfd);
+    // handle error if request failed
     handle_error(response);
+   	// free allocated memory
    	free(message);
    	free(response);
    	free(cookies[0]);
@@ -377,15 +449,22 @@ void logout() {
 	}
 	char *message;
 	char *response;
-	char type[4] = "GET";
+	char type[4] = GET;	// request type
 	char **cookies = (char **) malloc(sizeof(char *));
 	cookies[0] = (char *) calloc(200, sizeof(char));
+	// add login cookie
 	strcpy(cookies[0], login_cookie);
+	// compute GET request
 	message = compute_request(type, url, LOGOUT_PATH, NULL, NULL, 0,
 		NULL, cookies, 1);
+	// send message to sever
     send_to_server(sockfd, message);
+    // get server response
     response = receive_from_server(sockfd);
+    // handle error if failed or delete credentials if successful
     if (handle_error(response) == 0) {
+    	// request was successful
+    	// get rid of stored credentials
     	free(login_cookie);
     	login_cookie = NULL;
     	if (jwt_token != NULL) {
@@ -393,6 +472,7 @@ void logout() {
     		jwt_token = NULL;
     	}
     }
+   	// free allocated memory
    	free(message);
    	free(response);
    	free(cookies[0]);
@@ -407,9 +487,12 @@ int handle_error(char *response) {
 		// request was successful
 		return 0;
 	} else {
+		// request failed
 		if (token[0] == '4') {
+			// bad request
 			char *aux;
 			aux = response;
+			// search for error message
 			while(strncmp(aux, "error", 5)) {
 				aux++;
 			}
